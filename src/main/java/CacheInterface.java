9a805a0b04e506ca8369be2af79db04331e14dfa
Yuri Borges <yuriborges@me.com>
2019-01-22 21:33:49 +0100

    Commenting code to make the application runnable for testing

--- src/main/java/CacheInterface.java
+++ src/main/java/CacheInterface.java
@@ -1,5 +1,5 @@
-interface CacheInterface<E> {
-
-    public add_variable();
-    public <K> remove_variable_by_key(K keyObj);
-}
+//interface CacheInterface<E> {
+//
+//    public add_variable();
+//    public <K> remove_variable_by_key(K keyObj);
+//}
